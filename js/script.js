7fdf672e52fabf28c13a5aa0d0844a48f6266d36
mbagolor <mbagolor@hawaii.edu>
2020-11-14 03:28:34 -1000

    week 10 part 3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
+
+// OVERLAY NAV MENU SHOW HIDE
 const mymenubutton = document.querySelector('.menu-button');
-const mysitenav = document.querySelector('.site-nav');
+const mysitenav = document.querySelector('.site-header .site-nav');
 
 mymenubutton.onclick = function () {
-  if (mysitenav.getAttribute('data-navstate') ==='open') {
-    mysitenav.setAttribute('data-navstate','closed');
- } else {
-   mysitenav.setAttribute('data-navstate','open');
-  }
-}
+    if (mysitenav.getAttribute('data-navstate') === 'open') {
+        mysitenav.setAttribute('data-navstate', 'closed')
+    } else {
+        mysitenav.setAttribute('data-navstate', 'open')
+    }
+};
+
+// REVEAL ON SCROLL JS
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute('data-sectionstate', 'active');
+        } else {
+            entry.target.setAttribute('data-sectionstate', 'inactive');
+        }
+    });
+});
+document.querySelectorAll('.animate-on-scroll').forEach((section) => {
+    myobserver.observe(section);
+});
